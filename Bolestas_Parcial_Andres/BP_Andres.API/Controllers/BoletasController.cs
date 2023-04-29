0c4949a5407dc16dc5c0b0e15676a1dee0364a25
aalvarezm <andresalvarezm963@gmail.com>
2023-04-28 21:08:55 -0500

    Creación de lista generica

---
+++ Bolestas_Parcial_Andres/BP_Andres.API/Controllers/BoletasController.cs
@@ -0,0 +1,46 @@
+﻿using BP_Andres.API.Data;
+using BP_Andres.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BP_Andres.API.Controllers
+{
+    [ApiController]
+    [Route("/api/boletas")]
+    public class BoletasController: ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BoletasController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //[HttpPost]
+        //public async Task<ActionResult> PostAsync(Boleta boleta){
+
+        //    _context.Boletas.Add(boleta);
+        //    await _context.SaveChangesAsync();
+        //    return Ok(boleta);
+
+        //}
+
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+            return Ok(await _context.Boletas.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var boleta = await _context.Boletas.FirstOrDefaultAsync(x => x.Id == id);
+            if(boleta == null)
+            {
+                return NotFound(); 
+            }
+            return Ok(boleta);
+        }
+
+    }
+}
--- Bolestas_Parcial_Andres/BP_Andres.WEB/Program.cs
+++ Bolestas_Parcial_Andres/BP_Andres.WEB/Program.cs
@@ -1,4 +1,5 @@
 using BP_Andres.WEB;
+using BP_Andres.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7282/") });
+builder.Services.AddScoped<IRepository, Repository>(); //Me crea una instancia nueva del objeto cada que se realiza una inyección
 
 await builder.Build().RunAsync();
